733c7f82b9b01feea788bddf192b88b0682aa1f0
Uusuff <uusuuff@gmail.com>
2022-05-24 16:59:01 +0300

    adding script with markup

--- script2.js
+++ script2.js
@@ -1,23 +1,29 @@
-let optionsDiv = document.querySelector('div.options'); 
-const newDiv = document.createElement('div');
-const  newBtn = document.createElement('button');
+const divConteiner = createElemDiv('container',"");
+const divTime = createElemDiv('time',"00:00:00");
+const divOptions = createElemDiv('options',"");
+const btnStart = createElemBtn('start',"Start");
+const btnStop = createElemBtn('stop hidden',"Stop");
+const btnReset = createElemBtn('reset hidden',"Reset");
+
+document.body.insertBefore(divConteiner,document.body.firstChild);
+document.querySelector('.container').appendChild(divTime);
+document.querySelector('.container').appendChild(divOptions);
+document.querySelector('.options').appendChild(btnStart);
+document.querySelector('.options').appendChild(btnStop);
+document.querySelector('.options').appendChild(btnReset);
 
 
-//let newE = parentElem.appendChild(createElemBody(newDiv,'prodol','prodol'));
-//console.log(newE);
 
-
-
-let element = createElemBody(newDiv,'prodol','prodol');
-//document.getElementsByTagName('body')[0].append(element);
-document.body.insertBefore(element, document.body.firstChild);
-
-
-
-
-function createElemBody(tag,clasName,text){
-	let element = tag;
+function createElemDiv(clasName,text){
+	let element = document.createElement('div');
 	element.className = clasName;
 	element.innerHTML = text;
 	return element;
-}
+};
+
+function createElemBtn(clasName,text){
+	let element = document.createElement('button');
+	element.className = clasName;
+	element.innerHTML = text;
+	return element;
+};
